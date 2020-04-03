7a6c1d0287b12b7dd313d5bf5ed9151f1366ab0f
Dhika Rizky <deka_r@outlook.com>
2020-04-04 03:48:56 +0800

    Added Room model

---
+++ src/models/Room.ts
@@ -0,0 +1,10 @@
+import { Entity, Column, BaseEntity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export default class Room extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  roomName: string;
+}
